a3ef31ec34e366f83ab8ce5c756c06112c7c0e59
Tanfeez30520 <t01auheed@gmail.com>
2026-03-22 15:01:20 +0530

    produrl

--- src/Api/auth.js
+++ src/Api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://gps-backend-p91d.onrender.com'
+// https://gps-backend-p91d.onrender.com
 const api = axios.create({
   baseURL: API_BASE_URL,
   timeout: 20000,
